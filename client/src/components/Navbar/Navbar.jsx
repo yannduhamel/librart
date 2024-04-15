5121e0815844f99e6b099e1009f8d90ce505fda5
Yann Duhamel <yann.duhamel@gmail.com>
2024-04-15 22:39:12 +0200

    feat: create Menu

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,12 @@
 import Logo from "../Logo/Logo";
+import Menu from "../Menu/Menu";
 import styles from "./Navbar.module.css";
 
 function Navbar() {
   return (
     <div className={styles.Navbar}>
       <Logo />
+      <Menu />
     </div>
   );
 }
