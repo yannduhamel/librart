75dfc5844c6f71bd7ce0a4d6952847f89d307ffa
Irene Sánchez <enerisan@gmail.com>
2024-04-16 17:30:47 +0200

    feat: create LicencePage

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,27 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import App from "./App";
+import CatalogPage from "./pages/CatalogPage/CatalogPage";
+import HomePage from "./pages/HomePage/HomePage";
+import LicencePage from "./pages/LicencePage/LicencePage";
 
 const router = createBrowserRouter([
   {
-    path: "/",
     element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/catalog",
+        element: <CatalogPage />,
+      },
+      {
+        path: "/licence",
+        element: <LicencePage />,
+      },
+    ],
   },
 ]);
 
