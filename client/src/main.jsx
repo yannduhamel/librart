d361961fb14d52de92318b051f73c8e2be5e3eca
Yann Duhamel <yann.duhamel@gmail.com>
2024-04-17 09:24:14 +0200

    feat: create cgu page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,14 +3,7 @@ import ReactDOM from "react-dom/client";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import App from "./App";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-]);
+const router = createBrowserRouter([{}]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
