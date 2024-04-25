c0aabdf84580dea9f863cda2c17bf57e7244eb1a
Irene Sánchez <enerisan@gmail.com>
2024-04-26 01:48:39 +0200

    fix: css licencePage

--- client/src/pages/LicencePage/LicencePage.jsx
+++ client/src/pages/LicencePage/LicencePage.jsx
@@ -2,16 +2,16 @@ import styles from "./LicencePage.module.css";
 
 function LicencePage() {
   return (
-    <div className={styles.main}>
-      <h1>Licence</h1>
-      <p className={styles.p}>
+    <div className={styles.licenceContainer}>
+      <h1 className={styles.mainTitle}>Licence</h1>
+      <p className={styles.licenceParagraph}>
         Toutes les images téléchargeables sur le site proviennent de l'API du
         Metropolitan Museum of Art (New York) ayant adopté une politique d'accès
         ouvert aux images du domaine public de leurs collections, les rendant
         disponibles pour être utilisées librement à des fins commerciales ou
         autres.
       </p>
-      <p className={styles.p}>
+      <p className={styles.licenceParagraph}>
         Par conséquent, les utilisateurs du site sont autorisés à télécharger,
         partager et réutiliser ces images sans aucune restriction.
       </p>
